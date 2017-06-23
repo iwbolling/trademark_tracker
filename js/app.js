3c811095fa137b4c8092f65980f446993d70377c
iwbolling <iwbolling@gmail.com>
2017-06-23 18:11:14 -0400

    learning ForerunnerDB and JsRender

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 // Trademark Tracker by Ian Wilson Bolling
 
 // DATABASE INITIALIZATION
+
+// ForerunnerDB Practice
+window.fdb = new ForerunnerDB();
+db = fdb.db("bobDB");
+var bobColl = db.collection("bobCollection")
+bobColl.insert([
+									{ name: "Bob Dobbs", hobby: "slack"},
+									{ name: "Bob Johnson", hobby: "manliness"},
+									{ name: "Bob Ross", hobby: "landscapes"}
+								]);
+console.log(bobColl);
+var entryTemplate = $.templates("{{>name}} {{>hobby}}")
+$("#testZone").html(
+	entryTemplate.render(bobColl._data)
+);
