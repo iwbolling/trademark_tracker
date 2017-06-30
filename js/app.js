9abc8702715b2bed33b2d26ab935a50adadbd7bc
iwbolling <iwbolling@gmail.com>
2017-06-30 09:45:30 -0400

    figured out ForerunnerDB dynamic updating and rendering

--- js/app.js
+++ js/app.js
@@ -1,18 +1,44 @@
 // Trademark Tracker by Ian Wilson Bolling
 
 // DATABASE INITIALIZATION
+var fdb = new ForerunnerDB();
+tmdb = fdb.db("trademarkDB");
+var tmColl = tmdb.collection("trademarkColl");
+tmColl.load();
 
-// ForerunnerDB Practice
-window.fdb = new ForerunnerDB();
-db = fdb.db("bobDB");
-var bobColl = db.collection("bobCollection")
-bobColl.insert([
-									{ name: "Bob Dobbs", hobby: "slack"},
-									{ name: "Bob Johnson", hobby: "manliness"},
-									{ name: "Bob Ross", hobby: "landscapes"}
-								]);
-console.log(bobColl);
-var entryTemplate = $.templates("{{>name}} {{>hobby}}")
-$("#testZone").html(
-	entryTemplate.render(bobColl._data)
+// DB Practice
+tmColl.insert([
+	{name: "Apex Engineering", regNum: 7142119341419282},
+	{name: "Blackburn Strategic Gold Group", regNum: 0424508093867784},
+	{name: "Cosmopolitan Wetware, Inc.", regNum: 5264857214691913}
+]);
+tmColl.insert({name: "Derumo Cyber Sciences", regNum: 9524504327476591})
+tmColl.save();
+console.log(tmColl._data);
+
+var entryTemplate = $.templates(
+	"<tr><td>{{>name}}</td><td>{{>regNum}}</td></tr>"
 );
+function saveAndRender(){
+	tmColl.save(function(){
+		$("#trademark-table_body").html(
+			entryTemplate.render(tmColl._data)
+		);
+	})
+}
+function renderTable(){
+	$("#trademark-table_body").html(
+		entryTemplate.render(tmColl._data)
+	);
+}
+saveAndRender();
+
+// Persistence Test
+let testBtn = document.querySelector(".testBtn");
+testBtn.addEventListener("click",function(){
+	tmColl.insert([{name: "TEST SUCCEEDED", regNum: 1}]);
+	tmColl.save(function(){
+		saveAndRender();
+	});
+	console.log(tmColl._data);
+});
